9ded48c4b25df46e8766a57d0ae900172dae4469
Solko <jegjagg@gmail.com>
2020-03-29 10:19:21 +0100

    Handles

---
+++ Assets/Scripts/Utility/FaceCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace BreakEdit.Utility
+{
+	[AddComponentMenu("BreakEdit/Utility/Face Camera")]
+	public class FaceCamera : MonoBehaviour
+	{
+		[Header("Axis Freezing")]
+		public bool freezeX = false;
+		public bool freezeY = false;
+		public bool freezeZ = false;
+		
+		// Components
+		private Transform cameraTransform;
+		
+		private void Awake() => cameraTransform = FindObjectOfType<Camera>().transform;
+		
+		private Vector3 rotation;
+		private void Update()
+		{
+			Transform t = transform;
+			rotation = t.localEulerAngles;
+			t.LookAt(cameraTransform);
+			
+			Vector3 eulerAngles = t.localEulerAngles;
+			eulerAngles = new Vector3
+			(
+				freezeX ? rotation.x : eulerAngles.x,
+				freezeY ? rotation.y : eulerAngles.y,
+				freezeZ ? rotation.z : eulerAngles.z
+			);
+			t.localEulerAngles = eulerAngles;
+		}
+	}
+}
---
+++ Assets/Scripts/WorldObjects/Brush.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BreakEdit.WorldObjects
+{
+	[AddComponentMenu("BreakEdit/World Objects/Brush")]
+	[RequireComponent(typeof(MeshFilter))]
+	public class Brush : MonoBehaviour
+	{
+		private Mesh mesh;
+		
+		private void Awake()
+		{
+			mesh = GetComponent<MeshFilter>().mesh;
+		}
+	}
+}
